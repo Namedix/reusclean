d24cf0ec09bf894cc2f14c351af130b326bce270
Bartek <namedix@gmail.com>
2024-10-12 15:01:41 +0200

    Add account button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 import React, {Suspense, useState} from 'react';
 import {Link} from 'react-router-dom';
 import MenuSvg from '~/assets/MenuSvg';
-import {ShoppingBagIcon} from '@heroicons/react/24/outline';
+import {ShoppingBagIcon, UsersIcon} from '@heroicons/react/24/outline';
 import type {CartApiQueryFragment} from 'storefrontapi.generated';
 import {Await} from '@remix-run/react';
 import Cart from '~/routes/cart';
@@ -17,6 +17,11 @@ const Header = ({cart}: HeaderProps) => {
   const toggleNavigation = () => {
     setOpenNavigation(!openNavigation);
   };
+
+  const handleAccountClick = () => {
+    window.open('https://reusclean.com/account', '_blank');
+  };
+
   return (
     <header className="w-full bg-white ">
       <div className="md:container animate-fade-in-up">
@@ -33,7 +38,7 @@ const Header = ({cart}: HeaderProps) => {
               openNavigation ? `pb-4 mt-6` : `hidden max-h-0`
             } md:flex left-0 right-0 bottom-0 items-center justify-center md:static md:opacity-100 md:max-h-max md:mx-auto transition-all duration-200 overflow-hidden`}
           >
-            <div className="relative z-2 flex flex-col justify-center gap-6 md:flex-row">
+            <div className="relative z-2 flex flex-col justify-center gap-6 md:flex-row pt-1">
               <Link to="/#starter-set">Zestaw startowy</Link>
               <Link to="/products">Produkty</Link>
               <Link to="/#how-it-work">Jak działa?</Link>
@@ -42,7 +47,13 @@ const Header = ({cart}: HeaderProps) => {
             </div>
           </nav>
           <div className="absolute top-7 right-4 md:relative md:top-0 md:right-0 md:pl-4">
-            <div className="flex gap-4 items-center">
+            <div className="flex gap-6 items-center">
+              <button
+                onClick={handleAccountClick}
+                className="text-color-textLight text-sm md:text-md whitespace-nowrap"
+              >
+                <UsersIcon className="h-5 w-5 text-color-text" />
+              </button>
               <button
                 onClick={() => setOpenCart(!openCart)}
                 className=" flex items-center text-color-textLight"
@@ -51,7 +62,7 @@ const Header = ({cart}: HeaderProps) => {
                 <Suspense fallback={<p>0</p>}>
                   <Await resolve={cart}>
                     {(cart) => (
-                      <span className="ml-1 text-sm font-medium text-color-text flex items-center">
+                      <span className="ml-1 pt-1 text-sm font-medium text-color-text flex items-center">
                         {cart?.totalQuantity ?? 0}
                       </span>
                     )}
