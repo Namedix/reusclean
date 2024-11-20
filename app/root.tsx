13c2d26bfced7b8e4fd6003a4742c7369662452e
Bartek <namedix@gmail.com>
2024-11-20 23:44:46 +0100

    Add google tag

--- app/root.tsx
+++ app/root.tsx
@@ -144,15 +144,34 @@ export function Layout({children}: {children?: React.ReactNode}) {
     );
   };
 
+  // Add GTM script as a raw HTML string
+  const gtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PJ4Z2CLX');`;
+
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: gtmScript,
+          }}
+        />
         <Meta />
         <Links />
       </head>
       <body>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJ4Z2CLX"
+                    height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+
         {data ? (
           <Analytics.Provider
             cart={data.cart}
