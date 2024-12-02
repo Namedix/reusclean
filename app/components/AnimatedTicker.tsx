37bf86092b04ca770c375fc76fe84d06c9543a57
Bartek <namedix@gmail.com>
2024-12-02 01:05:48 +0100

    Remove black friday

--- app/components/AnimatedTicker.tsx
+++ app/components/AnimatedTicker.tsx
@@ -1,14 +1,14 @@
 const AnimatedTicker: React.FC = () => {
   const message =
-    '• Black Week - darmowa wysyłka z kodem DOSTAWA • Black Week - darmowa wysyłka z kodem DOSTAWA';
+    '• Prezent na start - Gratis do pierwszych zamówień! • Prezent na start - Gratis do pierwszych zamówień!';
 
   return (
     <div className="ticker-container grow rounded-md flex items-center justify-center">
       <div className="ticker-content">
-        <span className="text-color-white text-sm px-4">{message}</span>
-        <span className="text-color-white text-sm px-4">{message}</span>
-        <span className="text-color-white text-sm px-4">{message}</span>
-        <span className="text-color-white text-sm px-4">{message}</span>
+        <span className="text-color-blue text-sm px-4">{message}</span>
+        <span className="text-color-blue text-sm px-4">{message}</span>
+        <span className="text-color-blue text-sm px-4">{message}</span>
+        <span className="text-color-blue text-sm px-4">{message}</span>
       </div>
     </div>
   );
